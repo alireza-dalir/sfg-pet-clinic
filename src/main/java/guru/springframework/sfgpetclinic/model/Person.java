ae59a3ab7b0e56d627af56bee9f253245975e6b4
alireza-dalir <alirezadalir1999@gmail.com>
2019-05-27 19:09:35 +0430

    adding POJOs . Closes #2 .

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+
+
+    private String fristName;
+    private String lastName;
+
+    public String getFristName() {
+        return fristName;
+    }
+
+    public void setFristName(String fristName) {
+        this.fristName = fristName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
