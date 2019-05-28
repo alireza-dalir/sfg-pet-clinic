d7ba76e55dbb591dfbc3668098b978310fb7cee4
alireza-dalir <alirezadelir1999@gmail.com>
2019-05-28 20:32:08 +0430

    adding index page and controller . Closes #8 .

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/IndexController.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+
+    @RequestMapping({"","/","index.html"})
+    public String index(){
+        return "index";
+    }
+
+}
