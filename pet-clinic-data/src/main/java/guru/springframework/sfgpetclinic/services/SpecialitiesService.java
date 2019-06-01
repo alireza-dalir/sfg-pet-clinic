c1dd511c352b665ef3f106eeba6f05ae2a9af1dd
alireza-dalir <alirezadelir1999@gmail.com>
2019-06-01 19:07:56 +0430

    done . closes #31 .

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality , Long> {
+
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality , Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public void delete(Speciality t) {
+        super.delete(t);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
