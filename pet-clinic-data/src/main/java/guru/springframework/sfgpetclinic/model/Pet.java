967bcbcd60c792e6a50d353ea0f1a65debd62fae
alireza-dalir <alirezadelir1999@gmail.com>
2019-06-02 23:47:21 +0430

    updated visit entity with jpa . closes #38 .

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Entity
@@ -21,6 +23,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL , mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -52,5 +57,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
