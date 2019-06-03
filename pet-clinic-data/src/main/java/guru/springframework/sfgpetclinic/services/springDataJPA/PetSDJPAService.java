6e28f8a4ed2ffb416a4a3ccb70b50179f604dc44
alireza-dalir <alirezadelir1999@gmail.com>
2019-06-03 17:35:35 +0430

    sdjpa added for pet . closes #43 .

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springDataJPA/PetSDJPAService.java
@@ -0,0 +1,49 @@
+package guru.springframework.sfgpetclinic.services.springDataJPA;
+
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJPAService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJPAService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
